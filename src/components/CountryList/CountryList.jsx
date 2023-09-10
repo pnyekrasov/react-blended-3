dd2b8aba3331a5e80cd878a286065f6e6d8c9900
Demian <demyan.korotchenko@gmail.com>
2023-09-10 11:34:21 +0200

    123

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,13 @@
 import { Grid, GridItem } from 'components';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
+  const location = useLocation();
   return (
     <Grid>
       {countries.map(({ country, id, flag }) => (
         <GridItem key={id}>
-          <Link to={`/country/${id}`}>
+          <Link to={`/country/${id}`} state={{ from: location }}>
             <img src={flag} alt={country} />
           </Link>
         </GridItem>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,12 +1,12 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [country, setCounty] = useState(null);
-
+  const location = useLocation();
   const { countryId } = useParams();
 
   useEffect(() => {
@@ -17,9 +17,23 @@ export const Country = () => {
       .catch(err => console.log(err))
       .finally(() => setIsLoading(false));
   }, [countryId]);
+  const backLink = location?.state?.from ?? '/';
 
   return (
     <Section>
+      <Link
+        to={backLink}
+        style={{
+          marginBottom: '60px',
+          color: '#f2f2f2',
+          letterSpacing: '0.06em',
+          textDecoration: 'underline',
+
+          borderColor: 'gray',
+        }}
+      >
+        Go Back
+      </Link>
       {isLoading && <Loader />}
       <Container>{country && <CountryInfo {...country} />}</Container>
     </Section>
