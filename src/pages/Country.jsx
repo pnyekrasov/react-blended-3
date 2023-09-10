163a4521bcd4758b65dc776d449f29453185650c
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-09-10 11:19:58 +0200

    1009-1

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,11 +1,27 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [country, setCounty] = useState(null);
+
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    if (!countryId) return;
+    setIsLoading(true);
+    fetchCountry(countryId)
+      .then(setCounty)
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false));
+  }, [countryId]);
+
   return (
     <Section>
-      <Container>
-        <h2>Country</h2>
-      </Container>
+      {isLoading && <Loader />}
+      <Container>{country && <CountryInfo {...country} />}</Container>
     </Section>
   );
 };
